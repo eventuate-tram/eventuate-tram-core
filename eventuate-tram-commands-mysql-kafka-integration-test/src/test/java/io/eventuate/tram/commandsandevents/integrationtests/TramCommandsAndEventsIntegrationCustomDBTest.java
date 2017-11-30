b89d0787310a51785d13bab29618a95f135296ca
Artem Sidorkin <Artem.Sidorkin@digitalarrowtech.com>
2017-11-30 10:27:35 +0300

    Updating libraries. Fixing tests.

--- eventuate-tram-commands-mysql-kafka-integration-test/src/test/java/io/eventuate/tram/commandsandevents/integrationtests/TramCommandsAndEventsIntegrationCustomDBTest.java
+++ eventuate-tram-commands-mysql-kafka-integration-test/src/test/java/io/eventuate/tram/commandsandevents/integrationtests/TramCommandsAndEventsIntegrationCustomDBTest.java
@@ -5,15 +5,8 @@ import io.eventuate.local.testutil.CustomDBTestConfiguration;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import javax.sql.DataSource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {TramCommandsAndEventsIntegrationTestConfiguration.class, CustomDBTestConfiguration.class})
