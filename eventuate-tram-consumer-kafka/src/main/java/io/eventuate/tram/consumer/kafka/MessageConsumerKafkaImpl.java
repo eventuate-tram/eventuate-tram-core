be2910248738a68d5f7523c0f72affa1f1d9fced
Gavin Chen <gavinyan.han@gmail.com>
2018-02-20 19:46:43 -0500

    change consumer callback implement

--- eventuate-tram-consumer-kafka/src/main/java/io/eventuate/tram/consumer/kafka/MessageConsumerKafkaImpl.java
+++ eventuate-tram-consumer-kafka/src/main/java/io/eventuate/tram/consumer/kafka/MessageConsumerKafkaImpl.java
@@ -50,14 +50,15 @@ public class MessageConsumerKafkaImpl implements MessageConsumer {
         try {
           logger.trace("Invoking handler {} {}", subscriberId, m.getId());
           handler.accept(m);
+          logger.trace("handled message {} {}", subscriberId, m.getId());
+          callback.accept(null, null);
         } catch (Throwable t) {
           logger.trace("Got exception {} {}", subscriberId, m.getId());
           logger.trace("Got exception ", t);
           callback.accept(null, t);
           return null;
         }
-        logger.trace("handled message {} {}", subscriberId, m.getId());
-        callback.accept(null, null);
+
         return null;
       });
     };
