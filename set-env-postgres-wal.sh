7590537b8952cde64f38ed9802f7783b12a97db7
Artem Sidorkin <Artem.Sidorkin@digitalarrowtech.com>
2018-04-02 11:04:34 +0300

    Refactoring env scripts.

--- set-env-postgres-wal.sh
+++ set-env-postgres-wal.sh
@@ -1,23 +1,9 @@
-if [ -z "$DOCKER_HOST_IP" ] ; then
-    if [ -z "$DOCKER_HOST" ] ; then
-      export DOCKER_HOST_IP=`hostname`
-    else
-      echo using ${DOCKER_HOST?}
-      XX=${DOCKER_HOST%\:*}
-      export DOCKER_HOST_IP=${XX#tcp\:\/\/}
-    fi
-fi
-
-echo DOCKER_HOST_IP is $DOCKER_HOST_IP
+. ./_set-env.sh
 
 export SPRING_DATASOURCE_URL=jdbc:postgresql://${DOCKER_HOST_IP}/eventuate
 export SPRING_DATASOURCE_USERNAME=eventuate
 export SPRING_DATASOURCE_PASSWORD=eventuate
-export EVENTUATELOCAL_CDC_DB_USER_NAME=eventuate
-export EVENTUATELOCAL_CDC_DB_PASSWORD=eventuate
 export SPRING_DATASOURCE_DRIVER_CLASS_NAME=org.postgresql.Driver
 export SPRING_DATASOURCE_TEST_ON_BORROW=true
 export SPRING_DATASOURCE_VALIDATION_QUERY="SELECT 1"
-export EVENTUATELOCAL_KAFKA_BOOTSTRAP_SERVERS=$DOCKER_HOST_IP:9092
-export EVENTUATELOCAL_ZOOKEEPER_CONNECTION_STRING=$DOCKER_HOST_IP:2181
 export SPRING_PROFILES_ACTIVE=PostgresWal
