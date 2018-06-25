e90b289b30ffeff8c983d43d8f302686fc473867
Chris Richardson <chris@chrisrichardson.net>
2018-06-25 07:39:02 -0700

    Fix DB command line client

--- postgres-cli.sh
+++ postgres-cli.sh
@@ -1,5 +1,7 @@
 #! /bin/bash -e
 
 docker run $* \
-   --name postgresterm --link $(echo ${PWD##*/} | sed -e 's/-//g')_postgres_1:postgres --rm postgres:9.6.5 \
-   sh -c 'export PGPASSWORD="$POSTGRES_ENV_POSTGRES_PASSWORD"; exec psql -h postgres -U "$POSTGRES_ENV_POSTGRES_USER" '
+   --name postgresterm --rm \
+   -e POSTGRES_ENV_POSTGRES_USER=eventuate -e POSTGRES_ENV_POSTGRES_PASSWORD=eventuate -e POSTGRES_HOST=$DOCKER_HOST_IP \
+   postgres:9.6.5 \
+   sh -c 'export PGPASSWORD="$POSTGRES_ENV_POSTGRES_PASSWORD"; exec psql -h $POSTGRES_HOST -U "$POSTGRES_ENV_POSTGRES_USER" '
