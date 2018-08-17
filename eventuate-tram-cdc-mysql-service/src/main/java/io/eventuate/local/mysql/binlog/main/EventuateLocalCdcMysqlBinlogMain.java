9395ee54822af9f1983af01b086b31e6171d1a32
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2018-08-17 19:09:37 +0300

    Reorganizing default pipeline configuration.

--- eventuate-tram-cdc-mysql-service/src/main/java/io/eventuate/local/mysql/binlog/main/EventuateLocalCdcMysqlBinlogMain.java
+++ eventuate-tram-cdc-mysql-service/src/main/java/io/eventuate/local/mysql/binlog/main/EventuateLocalCdcMysqlBinlogMain.java
@@ -1,19 +1,16 @@
 package io.eventuate.local.mysql.binlog.main;
 
-import io.eventuate.local.unified.cdc.CdcPipelineConfiguration;
-import io.eventuate.tram.cdc.mysql.connector.configuration.MessageTableChangesToDestinationsConfiguration;
 import io.eventuate.tram.cdc.mysql.connector.configuration.pipeline.CdcTramPipelineConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import(CdcTramPipelineConfiguration.class)
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})
 public class EventuateLocalCdcMysqlBinlogMain {
 
   public static void main(String[] args) {
