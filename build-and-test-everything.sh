308dc7a71c142aa212e54551e73a36a3211a3d84
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2018-08-28 18:18:45 +0300

    Added mariadb to circleci

--- build-and-test-everything.sh
+++ build-and-test-everything.sh
@@ -2,7 +2,7 @@
 
 set -o pipefail
 
-SCRIPTS="./build-and-test-all-mysql-binlog.sh ./build-and-test-all-cdc-mysql-binlog.sh ./build-and-test-all-cdc-postgres-polling.sh ./build-and-test-all-postgres-polling.sh ./build-and-test-all-cdc-postgres-wal.sh ./build-and-test-all-postgres-wal.sh"
+SCRIPTS="./build-and-test-all-mysql-binlog.sh ./build-and-test-all-cdc-mysql-binlog.sh ./build-and-test-all-mariadb-binlog.sh ./build-and-test-all-cdc-mariadb-binlog.sh ./build-and-test-all-cdc-postgres-polling.sh ./build-and-test-all-postgres-polling.sh ./build-and-test-all-cdc-postgres-wal.sh ./build-and-test-all-postgres-wal.sh"
 
 
 date > build-and-test-everything.log
