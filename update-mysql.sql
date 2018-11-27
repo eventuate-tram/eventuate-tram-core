85a0d4f8f3d8fbf7b2e8a08eb44b3ef672aea8d2
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2018-11-27 10:33:18 +0300

    Removed schema from update scripts.

--- update-mysql.sql
+++ update-mysql.sql
@@ -1,3 +1,3 @@
-CREATE TABLE eventuate.offset_store(client_name VARCHAR(255) NOT NULL PRIMARY KEY, serialized_offset VARCHAR(255));
-ALTER TABLE eventuate.message ADD creation_time BIGINT;
-ALTER TABLE eventuate.received_messages ADD creation_time BIGINT;
+CREATE TABLE offset_store(client_name VARCHAR(255) NOT NULL PRIMARY KEY, serialized_offset VARCHAR(255));
+ALTER TABLE message ADD creation_time BIGINT;
+ALTER TABLE received_messages ADD creation_time BIGINT;
