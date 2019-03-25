c553d5d0b09930ee9fc7dd367fa213d311c70914
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2019-03-25 21:02:21 +0300

    Fixed redis servers.

--- build-and-test-all-redis.sh
+++ build-and-test-all-redis.sh
@@ -4,6 +4,6 @@ set -e
 
 export PROFILE=Redis
 export BROKER=redis
-export REDIS_SERVERS=${DOCKER_HOST_IP}:6379,${DOCKER_HOST_IP}:6380,${DOCKER_HOST_IP}:6381
+export EVENTUATE_REDIS_SERVERS=${DOCKER_HOST_IP}:6379,${DOCKER_HOST_IP}:6380,${DOCKER_HOST_IP}:6381
 
 ./_build-and-test-all-message-broker.sh
