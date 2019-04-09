4ff5972453238952ed65b996c883544e134de79a
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2019-04-09 20:08:29 +0300

    additional try/catch for the coordinator

--- eventuate-tram-consumer-common/src/main/java/io/eventuate/tram/consumer/common/coordinator/Coordinator.java
+++ eventuate-tram-consumer-common/src/main/java/io/eventuate/tram/consumer/common/coordinator/Coordinator.java
@@ -96,10 +96,16 @@ public class Coordinator {
 
   private void onGroupMembersUpdated(Set<String> expectedGroupMembers) {
     logger.info("Updating group members, expectedGroupMembers : {}, subscriberId : {}, subscriptionId : {}", expectedGroupMembers, subscriberId, subscriptionId);
-    if (!partitionManager.isInitialized()) {
-      initializePartitionManager(expectedGroupMembers);
-    } else {
-      rebalance(expectedGroupMembers);
+
+    try {
+      if (!partitionManager.isInitialized()) {
+        initializePartitionManager(expectedGroupMembers);
+      } else {
+        rebalance(expectedGroupMembers);
+      }
+    } catch (Exception e) {
+      logger.error(e.getMessage(), e);
+      return;
     }
 
     previousGroupMembers = expectedGroupMembers;
