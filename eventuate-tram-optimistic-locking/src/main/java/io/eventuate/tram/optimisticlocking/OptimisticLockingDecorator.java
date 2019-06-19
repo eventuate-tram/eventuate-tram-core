449dee9bec2c5a31c5c58e97aa0999031f5ee978
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2019-06-19 13:35:24 +0300

    Refactoring

--- eventuate-tram-optimistic-locking/src/main/java/io/eventuate/tram/optimisticlocking/OptimisticLockingDecorator.java
+++ eventuate-tram-optimistic-locking/src/main/java/io/eventuate/tram/optimisticlocking/OptimisticLockingDecorator.java
@@ -4,17 +4,16 @@ import io.eventuate.tram.consumer.common.MessageHandlerDecorator;
 import io.eventuate.tram.consumer.common.MessageHandlerDecoratorChain;
 import io.eventuate.tram.consumer.common.SubscriberIdAndMessage;
 import org.springframework.core.Ordered;
-import org.springframework.orm.ObjectOptimisticLockingFailureException;
+import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
-
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 public class OptimisticLockingDecorator implements MessageHandlerDecorator, Ordered {
 
   @Override
-  @Retryable(value = ObjectOptimisticLockingFailureException.class,
+  @Retryable(value = OptimisticLockingFailureException.class,
           maxAttempts = 10,
           backoff = @Backoff(delay = 100))
   public void accept(SubscriberIdAndMessage subscriberIdAndMessage, MessageHandlerDecoratorChain messageHandlerDecoratorChain) {
--- eventuate-tram-optimistic-locking/src/main/java/io/eventuate/tram/optimisticlocking/OptimisticLockingDecoratorConfiguration.java
+++ eventuate-tram-optimistic-locking/src/main/java/io/eventuate/tram/optimisticlocking/OptimisticLockingDecoratorConfiguration.java
@@ -1,10 +1,12 @@
 package io.eventuate.tram.optimisticlocking;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class OptimisticLockingDecoratorConfiguration {
   @Bean
-  public OptimisticLockingDecorator customerOptimisticLockingDecorator() {
+  public OptimisticLockingDecorator optimisticLockingDecorator() {
     return new OptimisticLockingDecorator();
   }
 }
