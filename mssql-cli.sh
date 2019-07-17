5f64ef93c1423ef128cb3fd9b25d2bcbf1815525
Chris Richardson <chris@chrisrichardson.net>
2019-07-17 10:03:13 -0700

    Upgraded to newer Eventuate Local to fix MS SQL server SELECT TOP bug

--- mssql-cli.sh
+++ mssql-cli.sh
@@ -2,6 +2,6 @@
 
 docker run $* \
    --name mssqlterm --rm \
-   -e MSSQL_HOST=$DOCKER_HOST_IP \
+   -e MSSQL_HOST=${DOCKER_HOST_IP?} \
    mcr.microsoft.com/mssql/server:2017-latest  \
    sh -c 'exec /opt/mssql-tools/bin/sqlcmd -S "$MSSQL_HOST" -U SA -P "Eventuate123!"'
--- set-env-mssql-polling.sh
+++ set-env-mssql-polling.sh
@@ -1,6 +1,6 @@
 . ./_set-env.sh
 
-export SPRING_DATASOURCE_URL="jdbc:sqlserver://172.17.0.1:1433;databaseName=eventuate"
+export SPRING_DATASOURCE_URL="jdbc:sqlserver://${DOCKER_HOST_IP}:1433;databaseName=eventuate"
 export SPRING_DATASOURCE_USERNAME=sa
 export SPRING_DATASOURCE_PASSWORD=Eventuate123!
 export SPRING_DATASOURCE_DRIVER_CLASS_NAME=com.microsoft.sqlserver.jdbc.SQLServerDriver
