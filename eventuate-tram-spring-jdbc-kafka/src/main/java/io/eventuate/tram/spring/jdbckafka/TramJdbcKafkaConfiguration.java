240b5c9f55bb2c80d82de5ee5988470dfb6046bc
Didier Alberto Tabares Higuita <dtabaresh@psl.com.co>
2020-05-21 11:54:17 -0500

    Updates configuration to use kafka consumer factory

--- eventuate-tram-spring-jdbc-kafka/src/main/java/io/eventuate/tram/spring/jdbckafka/TramJdbcKafkaConfiguration.java
+++ eventuate-tram-spring-jdbc-kafka/src/main/java/io/eventuate/tram/spring/jdbckafka/TramJdbcKafkaConfiguration.java
@@ -1,12 +1,14 @@
 package io.eventuate.tram.spring.jdbckafka;
 
-import io.eventuate.tram.spring.consumer.kafka.EventuateTramKafkaMessageConsumerConfiguration;
-import io.eventuate.tram.spring.messaging.producer.jdbc.TramMessageProducerJdbcConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import io.eventuate.messaging.kafka.spring.consumer.KafkaConsumerFactoryConfiguration;
+import io.eventuate.tram.spring.consumer.kafka.EventuateTramKafkaMessageConsumerConfiguration;
+import io.eventuate.tram.spring.messaging.producer.jdbc.TramMessageProducerJdbcConfiguration;
 
 @Configuration
 @Import({TramMessageProducerJdbcConfiguration.class,
+        KafkaConsumerFactoryConfiguration.class,
         EventuateTramKafkaMessageConsumerConfiguration.class})
 public class TramJdbcKafkaConfiguration {
 }
