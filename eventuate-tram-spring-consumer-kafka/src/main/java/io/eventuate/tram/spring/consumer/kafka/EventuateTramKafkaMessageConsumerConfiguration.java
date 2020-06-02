43d90c14b126aff544490aa39e4010178783c2c8
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2020-06-02 20:31:48 +0300

    Used thread class-loader. https://github.com/eventuate-tram/eventuate-tram-sagas/issues/30

--- eventuate-tram-spring-consumer-kafka/src/main/java/io/eventuate/tram/spring/consumer/kafka/EventuateTramKafkaMessageConsumerConfiguration.java
+++ eventuate-tram-spring-consumer-kafka/src/main/java/io/eventuate/tram/spring/consumer/kafka/EventuateTramKafkaMessageConsumerConfiguration.java
@@ -1,6 +1,7 @@
 package io.eventuate.tram.spring.consumer.kafka;
 
 import io.eventuate.messaging.kafka.consumer.MessageConsumerKafkaImpl;
+import io.eventuate.messaging.kafka.spring.consumer.KafkaConsumerFactoryConfiguration;
 import io.eventuate.messaging.kafka.spring.consumer.MessageConsumerKafkaConfiguration;
 import io.eventuate.tram.consumer.common.MessageConsumerImplementation;
 import io.eventuate.tram.spring.consumer.common.TramConsumerCommonConfiguration;
@@ -11,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({TramConsumerCommonConfiguration.class, MessageConsumerKafkaConfiguration.class})
+@Import({TramConsumerCommonConfiguration.class, MessageConsumerKafkaConfiguration.class, KafkaConsumerFactoryConfiguration.class})
 public class EventuateTramKafkaMessageConsumerConfiguration {
 
   @Bean
