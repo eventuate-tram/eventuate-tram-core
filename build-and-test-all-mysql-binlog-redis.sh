3d32fdcecdacdea11062f30480a59e1020ec2abe
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2020-08-26 14:49:25 +0300

    #120: Remove env variables.

--- build-and-test-all-mysql-binlog-redis.sh
+++ build-and-test-all-mysql-binlog-redis.sh
@@ -5,5 +5,6 @@ set -e
 export DATABASE=mysql
 export MODE=binlog
 export BROKER=redis
+export SPRING_PROFILES_ACTIVE=Redis
 
 ./_build-and-test-all.sh
--- build-and-test-all-postgres-wal-rabbitmq.sh
+++ build-and-test-all-postgres-wal-rabbitmq.sh
@@ -6,5 +6,7 @@ export DATABASE=postgres
 export MODE=wal
 export BROKER=rabbitmq
 export COORDINATOR=zookeeper
+export MICRONAUT_ENVIRONMENTS=postgres
+export SPRING_PROFILES_ACTIVE=PostgresWal,RabbitMQ,postgres
 
 ./_build-and-test-all.sh
