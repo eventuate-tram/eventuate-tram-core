e37b635b957726970deba1211ecddc4f555d0d9d
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2021-03-18 14:16:17 +0300

    Added EventuateCommonJdbcConfiguration to TramConsumerJdbcAutoConfiguration

--- eventuate-tram-spring-consumer-jdbc/src/main/java/io/eventuate/tram/spring/consumer/jdbc/TramConsumerJdbcAutoConfiguration.java
+++ eventuate-tram-spring-consumer-jdbc/src/main/java/io/eventuate/tram/spring/consumer/jdbc/TramConsumerJdbcAutoConfiguration.java
@@ -3,6 +3,7 @@ package io.eventuate.tram.spring.consumer.jdbc;
 import io.eventuate.common.jdbc.EventuateJdbcStatementExecutor;
 import io.eventuate.common.jdbc.EventuateSchema;
 import io.eventuate.common.jdbc.EventuateTransactionTemplate;
+import io.eventuate.common.spring.jdbc.EventuateCommonJdbcOperationsConfiguration;
 import io.eventuate.common.spring.jdbc.EventuateSchemaConfiguration;
 import io.eventuate.common.spring.jdbc.sqldialect.SqlDialectConfiguration;
 import io.eventuate.common.jdbc.sqldialect.SqlDialectSelector;
@@ -13,12 +14,11 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.transaction.support.TransactionTemplate;
 
 @Configuration
 @Import({SqlDialectConfiguration.class,
-    EventuateSchemaConfiguration.class})
+    EventuateSchemaConfiguration.class,
+    EventuateCommonJdbcOperationsConfiguration.class})
 @ConditionalOnMissingBean(DuplicateMessageDetector.class)
 public class TramConsumerJdbcAutoConfiguration {
 
