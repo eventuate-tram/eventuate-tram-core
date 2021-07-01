699627a539a95d85b6944a404e8797a0c231555f
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2021-07-01 21:08:11 +0300

    added reactive messaging starter

---
+++ eventuate-tram-spring-reactive-messaging-starter/src/main/java/io/eventuate/tram/spring/reactive/messaging/autoconfigure/EventuateTramReactiveKafkaMessageConsumerAutoConfiguration.java
@@ -0,0 +1,15 @@
+package io.eventuate.tram.spring.reactive.messaging.autoconfigure;
+
+import io.eventuate.tram.consumer.common.MessageConsumerImplementation;
+import io.eventuate.tram.spring.reactive.consumer.kafka.EventuateTramReactiveKafkaMessageConsumerConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@ConditionalOnClass(EventuateTramReactiveKafkaMessageConsumerConfiguration.class)
+@ConditionalOnMissingBean(MessageConsumerImplementation.class)
+@Import(EventuateTramReactiveKafkaMessageConsumerConfiguration.class)
+public class EventuateTramReactiveKafkaMessageConsumerAutoConfiguration {
+}
