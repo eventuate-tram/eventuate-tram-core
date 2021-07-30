ff21edef625cecbb73333f8dd071a1eb3ebe06de
Artem Sidorkin <Artem.Sidorkin@digitalarrowtech.com>
2021-07-30 10:23:55 +0300

    Removed redundant conditions from reactive starters

--- eventuate-tram-spring-reactive-commands-starter/src/main/java/io/eventuate/tram/spring/reactive/commands/autoconfigure/EventuateReactiveTramCommandsAutoConfigure.java
+++ eventuate-tram-spring-reactive-commands-starter/src/main/java/io/eventuate/tram/spring/reactive/commands/autoconfigure/EventuateReactiveTramCommandsAutoConfigure.java
@@ -2,12 +2,10 @@ package io.eventuate.tram.spring.reactive.commands.autoconfigure;
 
 import io.eventuate.tram.spring.reactive.commands.consumer.ReactiveTramCommandConsumerConfiguration;
 import io.eventuate.tram.spring.reactive.commands.producer.ReactiveTramCommandProducerConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@ConditionalOnClass(ReactiveTramCommandConsumerConfiguration.class)
 @Import({ReactiveTramCommandConsumerConfiguration.class, ReactiveTramCommandProducerConfiguration.class})
 public class EventuateReactiveTramCommandsAutoConfigure {
 }
