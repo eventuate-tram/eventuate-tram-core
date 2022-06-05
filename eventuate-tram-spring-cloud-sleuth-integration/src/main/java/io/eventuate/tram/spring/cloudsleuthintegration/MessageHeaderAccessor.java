ecdaec2f89c6c4f5be6e3a4e20b4ec98523d15cb
Chris Richardson <chris@chrisrichardson.net>
2022-06-05 18:14:47 +0900

    Added dependencies for @PostConstruct + removed old eventuate-tram-spring-cloud-sleuth-integration

--- eventuate-tram-spring-cloud-sleuth-integration/src/main/java/io/eventuate/tram/spring/cloudsleuthintegration/MessageHeaderAccessor.java
+++
@@ -1,7 +0,0 @@
-package io.eventuate.tram.spring.cloudsleuthintegration;
-
-public interface MessageHeaderAccessor {
-  void put(String key, String value);
-  String get(String key);
-  void remove(String key);
-}
