b0246c487b32fd63448efdb565c457503cb6a9a2
Chris Richardson <chris@chrisrichardson.net>
2022-08-11 07:39:13 +0900

    Increased CircleCI job parallelism

--- _build-and-test-all.sh
+++ _build-and-test-all.sh
@@ -2,36 +2,48 @@
 
 set -e
 
-./gradlew $* testClasses
 
 docker="./gradlew ${DATABASE}${MODE}${BROKER}Compose"
 
-${docker}Down -P removeContainers=true
-${docker}Up
 
-./gradlew cleanTest build
+if [ -z "$TEST_DB_ID" ] ; then
 
-if [[ "${DATABASE}" == "mysql" ]] && [[ "${BROKER}" == "kafka" ]]; then
+./gradlew $* testClasses
+
+  ${docker}Down -P removeContainers=true
+
+  ${docker}Up
+
+  ./gradlew cleanTest build
+
+  if [[ "${DATABASE}" == "mysql" ]] && [[ "${BROKER}" == "kafka" ]]; then
+      ${docker}Down -P removeContainers=true
+      ${docker}Up
+
+      export EVENTUATE_DATABASE_SCHEMA=custom
+
+      ${docker}Up
+
+      export TEST_CUSTOM_DB=true
+
+      ./gradlew cleanTest :eventuate-tram-db-broker-integration-test:test --tests "io.eventuate.tram.broker.db.integrationtests.TramIntegrationCustomDBTest"
+      ./gradlew cleanTest :eventuate-tram-commands-db-broker-integration-test:test --tests "io.eventuate.tram.commands.db.broker.integrationtests.TramCommandsDBBrokerIntegrationCustomDBTest"
+  fi
+
+else
+  echo ==== TESTING DB_ID
+
+  ./gradlew $* testClasses
+
     ${docker}Down -P removeContainers=true
-    ${docker}Up
 
-    export EVENTUATE_DATABASE_SCHEMA=custom
+  export USE_DB_ID=true
+  export EVENTUATE_OUTBOX_ID=1
+  unset TEST_CUSTOM_DB
+  unset EVENTUATE_DATABASE_SCHEMA
+  ${docker}Up
 
-    ${docker}Up
-
-    export TEST_CUSTOM_DB=true
-
-    ./gradlew cleanTest :eventuate-tram-db-broker-integration-test:test --tests "io.eventuate.tram.broker.db.integrationtests.TramIntegrationCustomDBTest"
-    ./gradlew cleanTest :eventuate-tram-commands-db-broker-integration-test:test --tests "io.eventuate.tram.commands.db.broker.integrationtests.TramCommandsDBBrokerIntegrationCustomDBTest"
+  ./gradlew cleanTest build
 fi
-
-${docker}Down -P removeContainers=true
-export USE_DB_ID=true
-export EVENTUATE_OUTBOX_ID=1
-unset TEST_CUSTOM_DB
-unset EVENTUATE_DATABASE_SCHEMA
-${docker}Up
-
-./gradlew cleanTest build
 
 ${docker}Down  -P removeContainers=true
