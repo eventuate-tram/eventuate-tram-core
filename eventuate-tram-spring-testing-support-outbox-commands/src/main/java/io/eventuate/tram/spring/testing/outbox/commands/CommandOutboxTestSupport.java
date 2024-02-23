19e4fae928a65398cf9d74b69c386f52181670fa
Chris Richardson <chris@chrisrichardson.net>
2024-02-23 16:56:55 +0900

    Enhanced CommandOutboxTestSupport

--- eventuate-tram-spring-testing-support-outbox-commands/src/main/java/io/eventuate/tram/spring/testing/outbox/commands/CommandOutboxTestSupport.java
+++ eventuate-tram-spring-testing-support-outbox-commands/src/main/java/io/eventuate/tram/spring/testing/outbox/commands/CommandOutboxTestSupport.java
@@ -1,6 +1,8 @@
 package io.eventuate.tram.spring.testing.outbox.commands;
 
 import io.eventuate.common.json.mapper.JSonMapper;
+import io.eventuate.tram.commands.common.Command;
+import io.eventuate.tram.commands.common.CommandMessageHeaders;
 import io.eventuate.tram.commands.common.CommandReplyOutcome;
 import io.eventuate.tram.commands.common.ReplyMessageHeaders;
 import io.eventuate.tram.messaging.common.Message;
@@ -14,22 +16,36 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CommandOutboxTestSupport {
 
-  private JdbcTemplate jdbcTemplate;
+  private final JdbcTemplate jdbcTemplate;
 
   public CommandOutboxTestSupport(JdbcTemplate jdbcTemplate) {
     this.jdbcTemplate = jdbcTemplate;
   }
 
+  public <T extends Command> void assertCommandMessageSent(String channel, Class<T> commandMessageType) {
+
+    List<Message> messages = findMessagesSentToChannel(channel);
+
+    assertThat(messages)
+            .hasSize(1)
+            .allMatch(reply -> commandMessageType.getName().equals(reply.getRequiredHeader(CommandMessageHeaders.COMMAND_TYPE)));
+  }
   public void assertCommandReplyMessageSent(String channel) {
 
-    List<Message> messages = jdbcTemplate.query("select headers,payload from message where destination = ?", (rs, rowNum) -> {
-      String headers = rs.getString("headers");
-      String payload = rs.getString("payload");
-      return MessageBuilder.withPayload(payload).withExtraHeaders("", JSonMapper.fromJson(headers, Map.class)).build();
-    }, channel);
+    List<Message> messages = findMessagesSentToChannel(channel);
 
     assertThat(messages)
             .hasSize(1)
             .allMatch(reply -> CommandReplyOutcome.SUCCESS.name().equals(reply.getRequiredHeader(ReplyMessageHeaders.REPLY_OUTCOME)));
   }
+
+  private List<Message> findMessagesSentToChannel(String channel) {
+    return jdbcTemplate.query("select headers,payload from message where destination = ?", (rs, rowNum) -> {
+      String headers = rs.getString("headers");
+      String payload = rs.getString("payload");
+      return MessageBuilder.withPayload(payload).withExtraHeaders("", JSonMapper.fromJson(headers, Map.class)).build();
+    }, channel);
+  }
+
+
 }
