28a57e270f7a37bf5b0f4f30c06d8d3442b4bc35
Chris Richardson <Chris@ChrisRichardson.net>
2026-04-26 18:59:54 -0700

    Add @EnableEventuateTramFlywayMigration annotation

---
+++ eventuate-tram-spring-flyway/src/main/java/io/eventuate/tram/spring/flyway/EnableEventuateTramFlywayMigration.java
@@ -0,0 +1,14 @@
+package io.eventuate.tram.spring.flyway;
+
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Import(EventuateTramFlywayMigrationConfiguration.class)
+public @interface EnableEventuateTramFlywayMigration {
+}
--- eventuate-tram-spring-flyway/src/test/java/io/eventuate/tram/spring/flyway/EventuateTramFlywayMigrationTest.java
+++ eventuate-tram-spring-flyway/src/test/java/io/eventuate/tram/spring/flyway/EventuateTramFlywayMigrationTest.java
@@ -10,7 +10,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 
@@ -27,7 +27,7 @@ public class EventuateTramFlywayMigrationTest {
 
   @Configuration
   @EnableAutoConfiguration
-  @Import(EventuateTramFlywayMigrationConfiguration.class)
+  @EnableEventuateTramFlywayMigration
   public static class Config {
 
     @Bean
